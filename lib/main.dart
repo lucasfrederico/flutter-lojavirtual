32a94b5b8f7651372573475390e3d4838e6cbd7c
Lucas Rodrigo Frederico <55282018+lucasfrederico@users.noreply.github.com>
2021-01-07 20:36:13 -0300

    feat: navegando entre páginas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lojavirtual/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,11 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Flutter's Clothing",
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
