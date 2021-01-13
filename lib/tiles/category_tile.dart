522a3d0723a1928985ae372e45ef2b372e037f6c
Lucas Rodrigo Frederico <55282018+lucasfrederico@users.noreply.github.com>
2021-01-13 20:56:00 -0300

    feat: Criando tela de produtos

--- lib/tiles/category_tile.dart
+++ lib/tiles/category_tile.dart
@@ -1,8 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_lojavirtual/screens/category_screen.dart';
 
 class CategoryTile extends StatelessWidget {
-
   final DocumentSnapshot snapshot;
 
   CategoryTile(this.snapshot);
@@ -18,7 +18,11 @@ class CategoryTile extends StatelessWidget {
       title: Text(snapshot.data()['title']),
       trailing: Icon(Icons.keyboard_arrow_right),
       onTap: () {
-
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => CategoryScreen(snapshot),
+          ),
+        );
       },
     );
   }
