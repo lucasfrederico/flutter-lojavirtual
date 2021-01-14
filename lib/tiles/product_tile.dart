6b9290efb4fc9f759d4164a353a6501c0c550e90
Lucas Rodrigo Frederico <55282018+lucasfrederico@users.noreply.github.com>
2021-01-13 21:24:43 -0300

    feat: grade e lista de produtos

---
+++ lib/tiles/product_tile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_lojavirtual/datas/product_data.dart';
+
+class ProductTile extends StatelessWidget {
+  final String type;
+  final ProductData productData;
+
+  ProductTile(this.type, this.productData);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
