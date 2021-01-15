aee6f56d79db1e7cacf0d9a2162b013ea6df5633
Lucas Rodrigo Frederico <55282018+lucasfrederico@users.noreply.github.com>
2021-01-14 21:34:04 -0300

    feat: introdução ao ScopedModel

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_lojavirtual/screens/home_screen.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+import 'models/user_model.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,15 +15,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Flutter's Clothing",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: Color.fromARGB(255, 4, 125, 141),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return ScopedModel<UserModel>(
+      model: UserModel(),
+      child: MaterialApp(
+        title: "Flutter's Clothing",
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          primaryColor: Color.fromARGB(255, 4, 125, 141),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
     );
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lojavirtual/models/user_model.dart';
 import 'package:flutter_lojavirtual/screens/signup_screen.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class LoginScreen extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
@@ -29,64 +31,77 @@ class LoginScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.all(16.0),
-          children: [
-            TextFormField(
-              decoration: InputDecoration(hintText: 'E-mail'),
-              keyboardType: TextInputType.emailAddress,
-              validator: (text) {
-                if (text.isEmpty || !text.contains("@")) {
-                  return 'E-mail inválido';
-                }
-              },
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Senha'),
-              obscureText: true,
-              validator: (text) {
-                if (text.isEmpty || text.length < 6) {
-                  return 'Senha inválida';
-                }
-              },
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: FlatButton(
-                onPressed: () {},
-                child: Text(
-                  'Esqueci minha senha',
-                  textAlign: TextAlign.right,
-                ),
-                padding: EdgeInsets.zero,
-              ),
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            SizedBox(
-              height: 44.0,
-              child: RaisedButton(
-                child: Text(
-                  'Criar Conta',
-                  style: TextStyle(
-                    fontSize: 18.0,
+      body: ScopedModelDescendant<UserModel>(
+        builder: (context, child, model) {
+          if (model.isLoading) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else
+            return Form(
+              key: _formKey,
+              child: ListView(
+                padding: EdgeInsets.all(16.0),
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(hintText: 'E-mail'),
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (text) {
+                      if (text.isEmpty || !text.contains("@")) {
+                        return 'E-mail inválido';
+                      }
+                    },
                   ),
-                ),
-                textColor: Colors.white,
-                color: Theme.of(context).primaryColor,
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {}
-                },
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(hintText: 'Senha'),
+                    obscureText: true,
+                    validator: (text) {
+                      if (text.isEmpty || text.length < 6) {
+                        return 'Senha inválida';
+                      }
+                    },
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: FlatButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Esqueci minha senha',
+                        textAlign: TextAlign.right,
+                      ),
+                      padding: EdgeInsets.zero,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  SizedBox(
+                    height: 44.0,
+                    child: RaisedButton(
+                      child: Text(
+                        'Criar Conta',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        ),
+                      ),
+                      textColor: Colors.white,
+                      color: Theme.of(context).primaryColor,
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+
+                        }
+
+                        model.signIn();
+                      },
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
+            );
+        },
       ),
     );
   }
