a3530dbd76bc1788afbdad79db469f99c7b29644
Lucas Rodrigo Frederico <55282018+lucasfrederico@users.noreply.github.com>
2021-01-14 21:46:38 -0300

    feat: código para criar usuário

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,9 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class UserModel extends Model {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  UserCredential firebaseUser;
+  Map<String, dynamic> userData = Map();
+
   bool isLoading = false;
 
-  void signUp() {}
+  void signUp({
+    @required Map<String, dynamic> userData,
+    @required String pass,
+    @required VoidCallback onSuccess,
+    @required VoidCallback onFail,
+  }) {
+    isLoading = true;
+    notifyListeners();
+
+    _auth
+        .createUserWithEmailAndPassword(
+            email: userData['email'], password: pass)
+        .then((user) async {
+      firebaseUser = user;
+
+      await _saveUserData(userData);
+
+      onSuccess();
+      isLoading = false;
+      notifyListeners();
+    }).catchError((error) {
+      onFail();
+      isLoading = false;
+      notifyListeners();
+    });
+  }
 
   void signIn() async {
     isLoading = true;
@@ -18,4 +51,12 @@ class UserModel extends Model {
   void recoveryPass() {}
 
   bool isLoggedIn() {}
+
+  Future<Null> _saveUserData(Map<String, dynamic> userData) async {
+    this.userData = userData;
+    await Firestore.instance
+        .collection('users')
+        .document(firebaseUser.user.uid)
+        .setData(userData);
+  }
 }
