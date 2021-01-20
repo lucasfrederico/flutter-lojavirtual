0944d10c9c32b6fedc10f4e82c24c6d33857a0b3
Lucas Rodrigo Frederico <55282018+lucasfrederico@users.noreply.github.com>
2021-01-20 20:43:17 -0300

    feat: mostrando os itens no carrinho

--- lib/tiles/cart_tile.dart
+++ lib/tiles/cart_tile.dart
@@ -10,31 +10,100 @@ class CartTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     Widget _buildContent() {
-      return 
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.all(8.0),
+            width: 120.0,
+            child: Image.network(
+              cartProduct.productData.images[0],
+              fit: BoxFit.cover,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    cartProduct.productData.title,
+                    style: TextStyle(
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Text(
+                    'Tamanho: ${cartProduct.size}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  Text(
+                    'R\$  ${cartProduct.productData.price.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.remove),
+                        color: Theme.of(context).primaryColor,
+                        onPressed: cartProduct.quantity > 1 ? () {} : null,
+                      ),
+                      Text(cartProduct.quantity.toString()),
+                      IconButton(
+                        icon: Icon(Icons.add),
+                        color: Theme.of(context).primaryColor,
+                        onPressed: () {},
+                      ),
+                      FlatButton(
+                        child: Text('Remover'),
+                        textColor: Colors.grey[500],
+                        onPressed: () {},
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      );
     }
 
     return Card(
       margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-      child: cartProduct.productData == null ? FutureBuilder<DocumentSnapshot>(
-        future: Firestore.instance.collection('products').document(
-            cartProduct.category).collection('items')
-            .document(cartProduct.pid)
-            .get(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            cartProduct.productData = ProductData.fromDocument(snapshot.data);
-            return _buildContent();
-          } else {
-            return Container(
-              height: 70.0,
-              child: CircularProgressIndicator(),
-              alignment: Alignment.center,
-            );
-          }
-        },
-      ) : _buildContent(),
+      child: cartProduct.productData == null
+          ? FutureBuilder<DocumentSnapshot>(
+              future: Firestore.instance
+                  .collection('products')
+                  .document(cartProduct.category)
+                  .collection('items')
+                  .document(cartProduct.pid)
+                  .get(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  cartProduct.productData =
+                      ProductData.fromDocument(snapshot.data);
+                  return _buildContent();
+                } else {
+                  return Container(
+                    height: 70.0,
+                    child: CircularProgressIndicator(),
+                    alignment: Alignment.center,
+                  );
+                }
+              },
+            )
+          : _buildContent(),
     );
   }
 }
