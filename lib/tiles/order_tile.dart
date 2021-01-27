ac16e3d24af258dcc49e553d8d16e8fa7503586e
Lucas Rodrigo Frederico <55282018+lucasfrederico@users.noreply.github.com>
2021-01-26 21:11:58 -0300

    feat: acompanhando o status do pedido

--- lib/tiles/order_tile.dart
+++ lib/tiles/order_tile.dart
@@ -27,6 +27,8 @@ class OrderTile extends StatelessWidget {
               );
             }
 
+            int currentStatus = snapshot.data['status'];
+
             return Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -39,7 +41,37 @@ class OrderTile extends StatelessWidget {
                 SizedBox(
                   height: 4.0,
                 ),
-                Text(_buildProductsText(snapshot.data))
+                Text(_buildProductsText(snapshot.data)),
+                SizedBox(
+                  height: 4.0,
+                ),
+                Text(
+                  'Status do pedido:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 4.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _buildCircle('1', 'Preparação', currentStatus, 1),
+                    Container(
+                      height: 1.0,
+                      width: 40.0,
+                      color: Colors.grey[500],
+                    ),
+                    _buildCircle('2', 'Transporte', currentStatus, 2),
+                    Container(
+                      height: 1.0,
+                      width: 40.0,
+                      color: Colors.grey[500],
+                    ),
+                    _buildCircle('3', 'Entrega', currentStatus, 3)
+                  ],
+                )
               ],
             );
           },
@@ -50,11 +82,56 @@ class OrderTile extends StatelessWidget {
 
   String _buildProductsText(DocumentSnapshot snapshot) {
     String text = 'Descrição:\n';
-    for(LinkedHashMap product in snapshot.data()['products']) {
-      text += '${product['quantity']}x ${product['product']['title']} (R\$ ${product['product']['price'].toString()})\n';
+    for (LinkedHashMap product in snapshot.data()['products']) {
+      text +=
+          '${product['quantity']}x ${product['product']['title']} (R\$ ${product['product']['price'].toString()})\n';
     }
     text += 'Total: R\$ ${snapshot.data()['totalPrice'].toString()}';
 
     return text;
+  }
+
+  Widget _buildCircle(
+      String title, String subTitle, int currentStatus, int thisStatus) {
+    Color backColor;
+    Widget child;
+
+    if (currentStatus < thisStatus) {
+      backColor = Colors.grey[500];
+      child = Text(
+        title,
+        style: TextStyle(color: Colors.white),
+      );
+    } else if (currentStatus == thisStatus) {
+      backColor = Colors.blue;
+      child = Stack(
+        alignment: Alignment.center,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          ),
+        ],
+      );
+    } else {
+      backColor = Colors.green;
+      child = Icon(Icons.check, color: Colors.white,);
+    }
+
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 20,
+          backgroundColor: backColor,
+          child: child,
+        ),
+        Text(subTitle)
+      ],
+    );
   }
 }
